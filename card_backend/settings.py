2f8765e494b761042c4cbc7500d353bd358fda0c
fierylion <mawalladaniel2018@gmail.com>
2023-08-25 00:17:32 +0300

    changes

--- card_backend/settings.py
+++ card_backend/settings.py
@@ -145,7 +145,7 @@ CHANNEL_LAYERS = {
     'default':{
         'BACKEND':'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts":[('REDIS_NETWORK', 6379)],
+            "hosts":[('redis', 6379)],
         },
     },
 }

9e3262c6ecdb14a9e91a823aee7e1a259abfa13e
fierylion <mawalladaniel2018@gmail.com>
2023-08-30 22:10:18 +0300

    checking

--- card_backend/settings.py
+++ card_backend/settings.py
@@ -16,6 +16,7 @@ load_dotenv()
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+print(os.getenv('HOST'))
 
 
 # Quick-start development settings - unsuitable for production

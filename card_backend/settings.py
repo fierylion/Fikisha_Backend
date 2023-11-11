b9671b83ac8c8984ad96d9e1ca89a6a16e2e1c83
Daniel Mawalla <mawalladaniel2018@gmail.com>
2023-11-11 22:13:00 +0300

    changes

--- card_backend/settings.py
+++ card_backend/settings.py
@@ -146,7 +146,7 @@ CHANNEL_LAYERS = {
     'default':{
         'BACKEND':'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts":[('localhost',6379)],
+            "hosts":[('redis',6379)],
         },
     },
 }

be7d6f693b7261d7d3324c923be114f73570e17d
Daniel Mawalla <mawalladaniel2018@gmail.com>
2023-11-11 20:39:41 +0300

    changes

--- card_backend/asgi.py
+++ card_backend/asgi.py
@@ -6,6 +6,9 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
+import django
+
+django.setup()
 
 import os
 from channels.routing import ProtocolTypeRouter, URLRouter
